50eb82123da93e2b69a50d9b88ece714f4213251
Musa <tamzid101@gmail.com>
2019-05-10 05:44:51 +0600

    cmd argument added.

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,24 @@ extern crate regex;
 mod sql_parser;
 mod csv_helper;
 use std::process;
+use std::env;
 
 fn main() {
 
 
-    let path = "./uspop.csv";
-    let sql = "SELECT MIN(Population)";
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage: 'csv path' 'SQL'");
+        process::exit(1);
+    }
+
+    let path = &args[1];
+    let sql =  &args[2];
 
     if !sql_parser::is_valid(sql) {
         println!("Its not a valid sql");
-        process::exit(1);
+        process::exit(2);
     } 
 
     let colname : &str = sql_parser::get_column(sql);
@@ -23,12 +31,12 @@ fn main() {
         csv_helper::count(colname , path);
     } else if sql.contains("AVG") {
         csv_helper::avg(colname , path);
-
     } else if sql.contains("MAX") {
         csv_helper::max(colname , path);
     } else if sql.contains("MIN") {
         csv_helper::min(colname , path);
-
+    } else {
+        println!("Invalid");
     }
 
 }
