ad6475e4fc151d2119677a4fd586a76b473121cf
Musa <tamzid101@gmail.com>
2019-05-11 17:26:57 +0600

    comment added in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# this file is used by docker file.
 ./target/debug/csv_sql "./uspop.csv"  "SELECT COUNT(Population)"
 ./target/debug/csv_sql "./uspop.csv"  "SELECT MAX(Population)"
 ./target/debug/csv_sql "./uspop.csv"  "SELECT MIN(Population)"
